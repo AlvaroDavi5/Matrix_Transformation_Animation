703fe5f0906344894f04436f18c88642768ee340
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-04-29 22:17:32 -0300

    stl files uploaded

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+
+This code has created by Alvaro Davi using STL models avaliable on:
+	https://nasa3d.arc.nasa.gov/models
+
+
+
+Segundo Trabalho: Animacao Tridimensional usando Transformacoes Matriciais
+
+Alvaro Davi S. Alves - 2020101874
+
+Expressao Grafica para Engenharia
+
+Universidade Federal do Espirito Santo
+
+"""
+
+
+''' import libs '''
+import numpy as np # numeric manipulations, constants and functions
+ # scientific methods and functions
+import matplotlib.pyplot as plt # graphics and math expressions plot
+#%matplotlib inline # show plots on jupyter notebook line
+from mpl_toolkits.mplot3d import Axes3D, art3d # class and module to 3D projection
+from math import sin, cos, tan # trigonometric functions
+from stl import mesh # .stl files read
+#
+
+
+
